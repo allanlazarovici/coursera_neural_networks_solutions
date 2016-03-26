6d212bd07d82b71e7b4b6690f30cc81ace914406
Allan Lazarovici <allanlazarovici@Allans-iMac.local>
2016-03-26 18:07:03 -0400

    Committing Assignment 4 through configuration_goodness_gradient.m

---
+++ Assignment4/configuration_goodness.m
@@ -0,0 +1,16 @@
+function G = configuration_goodness(rbm_w, visible_state, hidden_state)
+% <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
+% <visible_state> is a binary matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
+% <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
+% This returns a scalar: the mean over cases of the goodness (negative energy) of the described configurations.
+  G = mean(diag( hidden_state'*rbm_w*visible_state ));
+
+  %G = 0;
+  %m = size(visible_state, 2);
+
+  %for i = 1:m
+  %  G += hidden_state(:, i)'*rbm_w*visible_state(:, i);
+  %end
+
+  %G = G/m;
+end
